20c1d3d73e7d1bcb27ed361ca243fc56c7d210c3
RamalingamRam <135614315+RamalingamRam@users.noreply.github.com>
2024-10-15 20:57:32 -0700

    This is the initial commit

--- kavigai/python/recommender.py
+++ kavigai/python/recommender.py
@@ -1,64 +1,8 @@
-
-from flask import Flask, render_template, request
-from tqdm import tqdm
-from sentence_transformers import SentenceTransformer
-from openai import OpenAI
 from pymongo import MongoClient
-import numpy as np
-from scipy.spatial.distance import cdist
-import pandas as pd
-import dotenv
-
-import os
-
-
-# openai.api_key = os.getenv('OPENAI_API_KEY')
-OAIclient = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['kavigai_rag']
 collection = db['goal_templates']
-
-from tqdm import tqdm
-from sentence_transformers import CrossEncoder
-from sentence_transformers import SentenceTransformer
-import numpy as np
-
-from transformers import AutoTokenizer
-
-
-# # Load a pre-trained embedding model
-model = SentenceTransformer('all-MiniLM-L6-v2')
-# model
-
-# Retrieve documents from MongoDB
-documents = collection.find()
-goal_templates_embeddings = []
-
-
-# Generate embeddings and store them in MongoDB
-for doc in documents:
-    goal_template_name = doc.get('goal template', '') # avoid error when field is missing
-    description = doc['description']  #will be an error when the field is missing
-    price = doc['price']
-    category = doc.get('category', '')
-    
-    # Concatenate title and content (or other fields)
-    combined_text = f"{goal_template_name}. {description}, {price}. {category}"
-    embedding = model.encode(combined_text)
-    goal_templates_embeddings.append({'_id': doc['_id'], 'goal_templates_embeddings': embedding.tolist()})
-
-# # Insert embeddings into a separate collection
-# if goal_templates_embeddings !exist ont insert 
-    # db.drop_collection('goal_templates_embeddings')
-goal_templates_embedding_collection = db['goal_templates_embeddings']
-######## goal_templates_embedding_collection.insert_many(goal_templates_embeddings)
-print("Document embeddings inserted successfully!")
-
-
 
 from scipy.spatial.distance import cdist
 import numpy as np
@@ -84,8 +28,17 @@ def retrieve_documents(query, top_k=3):
 
 
 
-# Function to retrieve relevant documents from MongoDB using embeddings
+import os
+import openai
+from openai import OpenAI
 
+OAIclient = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+
+# Function to retrieve relevant documents from MongoDB using embeddings
 def generate_response(query, documents):
     # Prepare the prompt for OpenAI
     prompt = f"Query: {query}\n\n"
@@ -95,38 +48,17 @@ def generate_response(query, documents):
     messages = [
     {"role": "system", "content": f"Find most relevernt documents for the {query}:"}
     ]
-    print(f'prompt: {messages}')
-
- # Add the user's question to the messages as a User Role
-    
-
+   
+    # Add the user's query to the messages as a User Role
     for doc in documents:
         messages.append({"role": "user", "content": f"\nGoal Template: {doc['goal template']}\nDescription: {doc['description']}\n"})
-        # prompt += f"\nGoal Template: {doc['goal template']}\nDescription: {doc['description']}\n"
-
-    print(f'prompt: {messages}')
-
+ 
     # Send the query to OpenAI
     response = OAIclient.chat.completions.create(
         model="gpt-4o-mini-2024-07-18",
         messages=messages
     )
-
-    # res1 = response['choices'][0]['message']['content']
-    # print(response['choices'][0]['message']['content'])
-    print(response.choices[0].message.content)
-
-
-    # res1 = response['choices'][0]['message']
-    # res2 = response['choices'][0]['message'][0]
-    # res3 = response['choices'][0]['message'][0]['content']
-
-    # res2 = response[0].choices[0].message[0].content[0]
-
-    # response.getJSONArray("choices").getJSONObject(0).getString("message")
-    # response[0]?.delta?.content || ""
     return response.choices[0].message.content
-
 
 # Main route for the web interface
 # Initialize Flask app
@@ -149,6 +81,3 @@ def index():
 
 if __name__ == '__main__': 
     app.run(debug=True)
-
-
-
